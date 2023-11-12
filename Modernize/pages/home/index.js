5b05def9f4d8d9ebb2b02fdbb3e76041ee87e5f0
DynastyKids <shabibox@outlook.com>
2023-11-13 00:32:37 +1100

    Homepage DIV card content update

--- Modernize/pages/home/index.js
+++ Modernize/pages/home/index.js
@@ -1,18 +1,320 @@
 const MongoClient = require('mongodb').MongoClient;
+const {ipcRenderer} = require('electron')
+const path = require('path')
 const {ServerApiVersion} = require('mongodb');
+
 const Storage = require("electron-store");
+const {error} = require("jquery");
 const newStorage = new Storage();
 
+const WeekNumber = require("weeknumber")
+const {weekNumberYearSun} = require("weeknumber");
+
 const uri = newStorage.get("mongoURI") ? newStorage.get("mongoURI") : "mongodb://localhost:27017"
-const targetDB = newStorage.get("mongoDB") ? newStorage.get("mongoDB") : "production"
+const dbname = newStorage.get("mongoDB") ? newStorage.get("mongoDB") : "production"
+
+// Preset Global Vaiable, Request data on page load
+let stockRecords = []
+let productsList =[]
+// Apex Charts
 
 
+document.addEventListener("DOMContentLoaded",async () => {
+    stockRecords = await getAllStockRecords();
+    productsList = await getProductsList();
 
-document.addEventListener("DOMContentLoaded",()=>{
-    if(checkDBConnection()){
+    console.log(stockRecords)
+    console.log(productsList)
 
+    // index.js Apex chart 1, Quarterly Breakup
+    //// 脚注标记
+    let quarterlyBreakupFootnote = document.querySelectorAll("#apex_quarterlyBreakup .col-12 .fs-2")
+    let last4Qtrs = getLast4Quarters()
+    for (let i = 0; i < last4Qtrs.length; i++) {
+        quarterlyBreakupFootnote[i].innerText = last4Qtrs[i]
     }
+
+    //// 获取季度数据
+    let quarterDatas= [0,0,0,0]
+    for (let i = 0; i < stockRecords.length; i++) {
+        for (let j = 0; j < productsList.length; j++) {
+            if (stockRecords[i].productCode === productsList[j].productCode && stockRecords[i].quantityUnit.toLowerCase() === "carton"
+                && productsList[j].hasOwnProperty("cartonQty")){
+                stockRecords[i].quantity = stockRecords[i].quantity * productsList[j].cartonQty
+            }
+        }
+        for (let j = 0; j < last4Qtrs.length; j++) {
+            if (last4Qtrs[j] === getFiscalQuarter(stockRecords[i].consumedTime) && stockRecords[i].hasOwnProperty("unitPrice")){
+                quarterDatas[j] += Math.round(parseInt(stockRecords[i].quantity) * parseFloat(stockRecords[i].unitPrice))
+            }
+        }
+    }
+
+    document.querySelector("#apex_quarterlyBreakup .card-title").textContent = "Quarterly Sale Breakup"
+    document.querySelector("#apex_quarterlyBreakup h4").textContent = `A$ ${new Intl.NumberFormat('en-AU').format(quarterDatas[0])}`
+    let posResult = !!(100 - Math.round(quarterDatas[0] / quarterDatas[1] * 100))//Positive growth ?
+    if (posResult){
+        document.querySelector("#diffWithLastYear span").className = "me-1 rounded-circle bg-light-success round-20 d-flex align-items-center justify-content-center"
+        document.querySelector("#diffWithLastYear span").innerHTML = `<i class="ti ti-arrow-up-right text-success"></i>`
+    } else {
+        document.querySelector("#diffWithLastYear span").className = "me-1 rounded-circle bg-light-danger round-20" +
+            " d-flex align-items-center justify-content-center"
+        document.querySelector("#diffWithLastYear span").innerHTML = `<i class="ti ti-arrow-down-right text-danger"></i>`
+    }
+    document.querySelector("#diffWithLastYear").append()
+    document.querySelector("#diffWithLastYear p").innerHTML = `${ posResult ? "+":"-"}${Math.abs(100-Math.round(quarterDatas[0]/quarterDatas[1]*100))}% than ${last4Qtrs[1]}`
+
+    
+
+    // Yearly Breakup Charts
+    // 按照季度计算整托盘产品的出货数量，1个托盘记为1件/或可以用货值替代，需要记录出货时候的货值
+    // 如果产品按照box/carton计算，则需要另外计算其托盘价值
+    var breakup = {
+        color: "#adb5bd",
+        series: quarterDatas,
+        labels: getLast4Quarters(),
+        chart: {
+            width: 175,
+            type: "donut",
+            fontFamily: "Plus Jakarta Sans', sans-serif",
+            foreColor: "#adb0bb",
+        },
+        plotOptions: {
+            pie: {
+                startAngle: 0,
+                endAngle: 360,
+                donut: {size: '75%',},
+            },
+        },
+        stroke: {show: false,},
+        dataLabels: {enabled: false,},
+        legend: {show: false,},
+        colors: ["#5b42ff", "#6b1fcf", "#be02f7","#ff004c"],
+        responsive: [
+            {
+                breakpoint: 991,
+                options: {
+                    chart: {width: 140,},
+                },
+            },
+        ],
+        tooltip: {
+            theme: "dark",
+            fillSeriesColor: false,
+        },
+    };
+    new ApexCharts(document.querySelector("#breakup"), breakup).render();
+
+
+
+
+    //Apex Chart 2  Stock Turnover Time
+    // Stock Turnover部分仅整托盘计算(出库时间-入库时间)/总托盘数量
+    let stockTurnOverArray = calcStockTurnover(stockRecords)  // Stock Turnover time will be calculated in days/hours
+    if (stockTurnOverArray.length>0){
+        let avgTime = 0
+        stockTurnOverArray.forEach(eachItem=>{
+            avgTime += eachItem.timediff
+        })
+        avgTime = Math.round(avgTime/stockTurnOverArray.length)
+
+        let lastMonthTurnoverRate = calcStockTurnover(stockRecords,25000,new Date(new Date().getFullYear(),new Date().getMonth(),0))  // Stock
+        let lastmonthAvg = 0
+        if (lastMonthTurnoverRate.length>0) {
+            lastMonthTurnoverRate.forEach(eachItem => {
+                lastmonthAvg += eachItem.timediff
+            })
+            lastmonthAvg = Math.round(lastmonthAvg / stockTurnOverArray.length)
+        }
+        console.log(avgTime,lastmonthAvg)
+
+        document.querySelector("#apex_turnoverChart h5").textContent = `Stock Turnover Time`
+        document.querySelector("#apex_turnoverChart h4").textContent = `${(avgTime/60/60/24).toFixed(1)} days*`
+        if(avgTime/lastmonthAvg-1 >0){
+            document.querySelector("#apex_turnoverChart .col-12 span").className = "me-2 rounded-circle bg-light-danger round-20 d-flex align-items-center justify-content-center"
+            document.querySelector("#apex_turnoverChart .col-12 span").innerHTML = `<i class="ti ti-arrow-down-right text-danger"></i>`
+        } else {
+            document.querySelector("#apex_turnoverChart .col-12 span").className = "me-2 rounded-circle bg-light-success round-20 d-flex align-items-center justify-content-center"
+            document.querySelector("#apex_turnoverChart .col-12 span").innerHTML = `<i class="ti ti-arrow-up-right text-success"></i>`
+        }
+        document.querySelector("#apex_turnoverChart .col-12 p")
+        document.querySelector("#apex_turnoverChart .col-12 p").textContent = `${(avgTime/lastmonthAvg-1) >0 ?  "+": "-"}${Math.abs(Math.round((avgTime/lastmonthAvg-1)*100))}% days compared on last month`
+    }
+
+    let turnOverChartData = RecentWeeksRemoveCount();
+    var turnOvers= {
+        chart: {
+            id: "sparkline3",
+            type: "area",
+            height: 75,
+            sparkline: {
+                enabled: true,
+            },
+            group: "sparklines",
+            fontFamily: "Plus Jakarta Sans', sans-serif",
+            foreColor: "#adb0bb",
+        },
+        series: [
+            {
+                name: "Pallets Out",
+                color: "#49BEFF",
+                type: "area",
+                data: turnOverChartData.pallets.reverse(),
+            },
+            {
+                name: "$ Value",
+                color: "#3200FF",
+                type: "line",
+                data: turnOverChartData.value.reverse(),
+            },
+        ],
+        stroke: {
+            curve: "smooth",
+            width: 1,
+        },
+        fill: {
+            colors: ["#f3feff"],
+            type: "solid",
+            opacity: [0.05,1],
+        },
+        yaxis:[
+            {title:{text:"Outbound pallets"}},
+            {opposite: true,title:{text:"Value"}}
+        ],
+        markers: {size: 0,},
+        tooltip: {
+            theme: "dark",
+            intersect: false,
+            fixed: {
+                enabled: true,
+                position: "right",
+            },
+            x: {show: false},
+            y: {show: false}
+        },
+    };
+    new ApexCharts(document.querySelector("#turnovers"), turnOvers).render();
 })
+
+function calcStockTurnover(stockData, limit = 25000, compareDate = new Date()){ // 该方法仅计算Turnover时长，默认仅计算最近的25000组数据，默认计算截止时间到现在
+    let turnOverArrayDiff=[]
+    // array of[{productCode:, timediff: byseconds}]
+    if (Array.isArray(stockData)){
+        for (let i = 0; i < stockData.length && i< limit; i++) {
+            const eachItem = stockData[i]
+            if (eachItem.hasOwnProperty("consumedTime")){
+                if(new Date(eachItem.consumedTime) > compareDate){
+                    continue;
+                }
+                if (eachItem.hasOwnProperty("consumed") && eachItem.consumed === 1 && eachItem.hasOwnProperty("loggingTime")){
+                    let logTime = new Date(eachItem.loggingTime);
+                    let removeTime = new Date(eachItem.consumedTime);
+                    turnOverArrayDiff.push({productCode:eachItem.productCode, consumedTime: eachItem.consumedTime, timediff: Math.floor(Math.abs(removeTime-logTime)/1000)})
+                }
+            }
+        }
+    } else {
+        throw error("Data format incorrect")
+    }
+    return turnOverArrayDiff
+}
+
+function isInWeek(YYWW, isoDateString){ // YY-2digit of year, WW - 2 digit of week number
+    const year = Math.floor(YYWW / 100) + 2000;
+    const week = YYWW % 100;
+    const date = new Date(isoDateString);
+    const dateYear = date.getFullYear();
+    const dateWeek = parseInt(`${weekNumberYearSun(date).year}`+`${weekNumberYearSun(date).week}`);
+
+    return year === dateYear && week === dateWeek;
+}
+
+function getFiscalQuarter(isoDateString = new Date()){
+    const date = new Date(isoDateString);
+    let quarter;
+    if (date.getMonth() < 3) { // JAN-MAR
+        quarter = 'Q1';
+    } else if ( date.getMonth() < 6) { // APR-JUN
+        quarter = 'Q2';
+    } else if ( date.getMonth() < 9) { // JUL-SEPT
+        quarter = 'Q3';
+    } else { // OCT-DEC
+        quarter = 'Q4';
+    }
+
+    return `${date.getFullYear().toString().substring(2)}${quarter}`;
+}
+
+function getLast4Quarters(isoDateString  = new Date()){
+    let currentYear = new Date(isoDateString).getFullYear();
+    const currentMonth = new Date(isoDateString).getMonth();
+
+    // 确定当前季度
+    let currentQuarter;
+    if (currentMonth < 3) {
+        currentQuarter = 1;
+    } else if (currentMonth < 6) {
+        currentQuarter = 2;
+    } else if (currentMonth < 9) {
+        currentQuarter = 3;
+    } else {
+        currentQuarter = 4;
+    }
+
+    // 生成包含当前季度和过去三个季度的数组
+    let quarters = [];
+    for (let i = 0; i < 4; i++) {
+        quarters.push(`${currentYear.toString().substring(2)}Q${currentQuarter}`);
+        currentQuarter -= 1; // 移动到上一个季度
+        if (currentQuarter === 0) { // 如果当前季度是Q1，转到上一年的Q4
+            currentQuarter = 4;
+            currentYear -= 1;
+        }
+    }
+    return quarters;
+}
+
+function RecentWeeksRemoveCount(weeks = 12) {// 计算最近连续的X个周的出货数量,至多计算25000条
+    let turnOverValueArrays = {pallets: [], value: [], week: []}  //Weeknum将会存放一个日期，在每次需要验证时候，由weekNumberYearSun转换验证
+    for (let i = 0; i < weeks; i++) {
+        if (i === 0) {
+            turnOverValueArrays.week.push(new Date())
+        } else {
+            turnOverValueArrays.week.push(new Date(turnOverValueArrays.week[i - 1].getTime() - 7 * 24 * 60 * 60 * 1000))
+            turnOverValueArrays.week[i-1] = weekNumberYearSun(turnOverValueArrays.week[i-1])
+        }
+        turnOverValueArrays.pallets.push(0);
+        turnOverValueArrays.value.push(0);
+    }
+    turnOverValueArrays.week[weeks-1] = weekNumberYearSun(turnOverValueArrays.week[weeks-1])
+    console.log(turnOverValueArrays)
+    for (let i = 0; i < stockRecords.length && i < 25000; i++) {
+        if (stockRecords[i].hasOwnProperty("consumedTime")) {
+            for (let j = 0; j < turnOverValueArrays.week.length; j++) {
+                if (turnOverValueArrays.week[j].week === weekNumberYearSun(stockRecords[i]["consumedTime"]).week &&
+                    turnOverValueArrays.week[j].year === weekNumberYearSun(stockRecords[i]["consumedTime"]).year) {
+                    for (let k = 0; k < productsList.length; k++) {
+                        if (productsList[k].productCode === stockRecords[i]['productCode'] && productsList[k].hasOwnProperty('cartonQty')&&
+                            (stockRecords[i]['quantityUnit'].toLowerCase().includes("carton") || stockRecords[i]['quantityUnit'].toLowerCase().includes("ctn"))){
+                            // console.log(stockRecords[i],productsList[k],stockRecords[i]['quantity'], turnOverValueArrays.value[j])
+                            stockRecords[i]['quantity'] = Math.round(parseInt(stockRecords[i]['quantity']) * parseInt(productsList[k]['cartonQty']))
+                            break;
+                        }
+                    }
+                    turnOverValueArrays.pallets[j] += 1
+                    if (isNaN(turnOverValueArrays.value[j])){
+                        turnOverValueArrays.value[j]=0
+                    }
+
+                    turnOverValueArrays.value[j] += Math.round(parseInt(stockRecords[i]['quantity']) * parseFloat(stockRecords[i]['unitPrice']))
+                }
+            }
+        }
+    }
+    console.log(turnOverValueArrays)
+    return turnOverValueArrays
+}
+
 
 $(function () {
     // Sales Overview 图表
@@ -97,118 +399,8 @@ $(function () {
                 }
             }
         ]
-
-
     };
-    var chart = new ApexCharts(document.querySelector("#chart"), chart);
-    chart.render();
-})
-
-$(function () {
-    // =====================================
-    // Breakup
-    // =====================================
-    var breakup = {
-        color: "#adb5bd",
-        series: [38, 40, 25],
-        labels: ["2022", "2021", "2020"],
-        chart: {
-            width: 180,
-            type: "donut",
-            fontFamily: "Plus Jakarta Sans', sans-serif",
-            foreColor: "#adb0bb",
-        },
-        plotOptions: {
-            pie: {
-                startAngle: 0,
-                endAngle: 360,
-                donut: {
-                    size: '75%',
-                },
-            },
-        },
-        stroke: {
-            show: false,
-        },
-
-        dataLabels: {
-            enabled: false,
-        },
-
-        legend: {
-            show: false,
-        },
-        colors: ["#5D87FF", "#ecf2ff", "#F9F9FD"],
-
-        responsive: [
-            {
-                breakpoint: 991,
-                options: {
-                    chart: {
-                        width: 150,
-                    },
-                },
-            },
-        ],
-        tooltip: {
-            theme: "dark",
-            fillSeriesColor: false,
-        },
-    };
-
-    var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
-    chart.render();
-})
-
-
-$(function () {
-    // =====================================
-    // Earning
-    // =====================================
-    var earning = {
-        chart: {
-            id: "sparkline3",
-            type: "area",
-            height: 60,
-            sparkline: {
-                enabled: true,
-            },
-            group: "sparklines",
-            fontFamily: "Plus Jakarta Sans', sans-serif",
-            foreColor: "#adb0bb",
-        },
-        series: [
-            {
-                name: "Earnings",
-                color: "#49BEFF",
-                data: [25, 66, 20, 40, 12, 58, 20],
-            },
-        ],
-        stroke: {
-            curve: "smooth",
-            width: 2,
-        },
-        fill: {
-            colors: ["#f3feff"],
-            type: "solid",
-            opacity: 0.05,
-        },
-
-        markers: {
-            size: 0,
-        },
-        tooltip: {
-            theme: "dark",
-            fixed: {
-                enabled: true,
-                position: "right",
-            },
-            x: {
-                show: false,
-            },
-        },
-    };
-    new ApexCharts(document.querySelector("#earning"), earning).render();
+    new ApexCharts(document.querySelector("#chart"), chart).render();
 })
 
 
@@ -220,31 +412,64 @@ async function checkDBConnection() {
     });
     try {
         await client.connect()
-        const sessions = client.db(targetDB);
-        let collections = await db.listCollections().toArray()
-
+        const sessions = client.db(dbname);
+        let collections = await sessions.listCollections().toArray()
+        return (collections.length >= 0 )
     } catch (e) {
         console.error(`Error on CheckDBConnection: ${e}`)
         return false;
     } finally {
         await client.close()
     }
+    return false
+}
 
-    return (collections.length >= 0 )
+async function getAllStockRecords(limit = 50000){
+    let client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1, useNewUrlParser: true, useUnifiedTopology: true
+        }
+    });
+    let result = []
+    try {
+        await client.connect()
+        let collections = client.db(dbname).collection("pollinglog");
+        result = await collections.find({}).sort({"consumedTime":-1,"loggingTime":1}).limit(limit).toArray()
+    } catch (e) {
+        console.error(`Error on CheckDBConnection: ${e}`)
+        return result
+    } finally {
+        await client.close()
+    }
+    return result
+}
+
+async function getProductsList(){
+    let client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1, useNewUrlParser: true, useUnifiedTopology: true
+        }
+    });
+    let result = []
+    try {
+        await client.connect()
+        let collections = client.db(dbname).collection("products");
+        result = await collections.find().sort({"productCode":1}).toArray()
+    } catch (e) {
+        console.error(`Error on CheckDBConnection: ${e}`)
+        return result
+    } finally {
+        await client.close()
+    }
+    return result
 }
 
 
-async function getSalesDataByYear(yyyymm) {
+async function getSalesDataByYear(date = new Date()) {
     // 根据用户输入获取指定的整个月的的sales进出数据，获得的数据按照月份分割，如果未传入参数则默认为当前月份
-    // 格式如202301 / 202210,年份要求最小为2000
-
     // 设定年份和时间
-    let year = new Date().getFullYear()
-    let month = new Date().getMonth()
-    if(!isNaN(yyyymm)){
-        year = parseInt(yyyymm/100)
-        month = yyyymm-year*100
-    }
+    let year = date.getFullYear()
+    let month = date.getMonth()
 
     let client = new MongoClient(uri, {
         serverApi: {
@@ -253,9 +478,8 @@ async function getSalesDataByYear(yyyymm) {
     });
     try {
         await client.connect()
-        const sessions = client.db(targetDB);
-        let collections = await db.listCollections().toArray()
-
+        let collections = await client.db(dbname).listCollections().toArray();
+        return (collections.length >= 0 )
     } catch (e) {
         console.error(`Error on CheckDBConnection: ${e}`)
         return false;
@@ -263,5 +487,5 @@ async function getSalesDataByYear(yyyymm) {
         await client.close()
     }
 
-    return (collections.length >= 0 )
+    return false
 }
